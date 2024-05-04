84d097a270abc7035dbee9aaf9d73b7884ab044e
V <116948898+raazrifany@users.noreply.github.com>
2024-05-04 13:17:48 +0700

    Create MainActivity.kt

---
+++ tugas-materi-3-bagian-1/UserInterface1/MainActivity.kt
@@ -0,0 +1,41 @@
+package simi.unikom.userinterface1
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var firstNameEditText: EditText
+    private lateinit var lastNameEditText: EditText
+    private lateinit var fullNameButton: Button
+    private lateinit var fullNameTextView: TextView
+    private fun initComponents() {
+        firstNameEditText = findViewById(R.id.firstNameEditText)
+        lastNameEditText = findViewById(R.id.lastNameEditText)
+        fullNameButton = findViewById(R.id.fullNameButton)
+        fullNameTextView = findViewById(R.id.fullNameTextView)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        initComponents()
+        fullNameButton.setOnClickListener {
+            val fullname = firstNameEditText.text.toString() + " " +
+                    lastNameEditText.text.toString()
+            fullNameTextView.text = "Full Name : $fullname"
+        }
+    }
+}
